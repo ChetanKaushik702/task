e0a4d7492772d6d2209e05610cb9c2f492eea466
Chetan Kaushik <chetan.kaushik@intelliticks.com>
2022-12-21 18:44:41 +0530

    Initial set-up done

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: "backend/config/.env" });
+
+const app = require("./app");
+
+const connectDB = require("./config/database");
+
+// handling uncaught errors
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err}`);
+  console.log("Shutting down the server due to uncaught exceptions.");
+  process.exit(1);
+});
+
+// connect database
+connectDB();
+
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// unhandled promise rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("Error:", err);
+  console.log("Shutting down the server due to unhandler promise rejections.");
+
+  server.close(() => {
+    process.exit(2);
+  });
+});
